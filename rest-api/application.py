63985d73e6f30b8637d8efb8e62e66c89164f398
Ahmed Eldably <ahmedeldabli@gmail.com>
2021-05-25 11:30:27 +0200

    Adding CORS support on all routes, for all origins and methods using flask_cors library

--- rest-api/application.py
+++ rest-api/application.py
@@ -1,11 +1,13 @@
 from flask import Flask, redirect, url_for, request, make_response, jsonify
 
 from BPTK_Py.server import BptkServer
+from flask_cors import CORS
 
 from model import bptk
 
 # Calling the BptkServer class
-application = BptkServer(__name__, bptk) 
+application = BptkServer(__name__, bptk)
+CORS(application)
 
 if __name__ == "__main__":
     application.run()
